3b4e6c55d98c00c244644dbb89d62d3aece2f89c
zhengdeqi <1587388110@qq.com>
2020-05-12 18:10:51 +0800

    Initial commit

---
+++ src/test/java/com/dln/lina/LinaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.dln.lina;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
